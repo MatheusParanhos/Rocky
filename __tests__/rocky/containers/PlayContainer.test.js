80bce4d8b3685d47a7015585886f73e8b7a00df2
Matheus <mtk_z@hotmail.com>
2018-08-26 21:01:35 -0300

    updated tests

--- __tests__/rocky/containers/PlayContainer.test.js
+++ __tests__/rocky/containers/PlayContainer.test.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import React from "react";
+import Enzyme from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
-import PlayContainer from '../../../src/rocky/play/containers/PlayContainer'
+import PlayContainer from "../../../src/rocky/play/containers/PlayContainer";
+import Result from "../../../src/rocky/play/components/Result";
 
 Enzyme.configure({ adapter: new Adapter() });
 var wrapper;
@@ -11,25 +12,113 @@ var wrapper;
 //     wrapper = Enzyme.shallow(<PlayContainer />);
 //   expect(wrapper).toMatchSnapshot();
 // })
-test('resetState', () => {
+test("resetState", () => {
   const props = {};
   const wrapper = Enzyme.shallow(<PlayContainer {...props} />);
   let action = wrapper.instance().resetState();
-  expect (action).toEqual(true);
-  
-  })
+  expect(action).toEqual(true);
+});
 
-  test('generateComputerChoice', () => {
-    const props = {};
-    const wrapper = Enzyme.shallow(<PlayContainer {...props} />);
-    let action = wrapper.instance().generateComputerChoice();
-    expect (action).toEqual(true);
-    
-    })  
-    test('jokenPo', () => {
+test("generateComputerChoice", () => {
+  const props = {};
+  const wrapper = Enzyme.shallow(<PlayContainer {...props} />);
+  let action = wrapper.instance().generateComputerChoice();
+  expect(action).toEqual(true);
+});
+
+jest.useFakeTimers();
+describe("PlayContainer Components", () => {
+  describe("jokenPo function", () => {
+    test("rock won's over scissors", () => {
       const props = {};
       const wrapper = Enzyme.shallow(<PlayContainer {...props} />);
-      let action = wrapper.instance().jokenPo();
-      expect (action).toEqual(true);
-      
-      })  
+      wrapper.instance().jokenPo("rock", "scissors");
+      expect(wrapper.find(Result).props().countdown).toEqual(3);
+      jest.runAllTimers();
+      expect(wrapper.find(Result).props().result).toEqual("Nice, you won!!");
+    });
+    test("rock draws with rock", () => {
+      const props = {};
+      const wrapper = Enzyme.shallow(<PlayContainer {...props} />);
+      wrapper.instance().jokenPo("rock", "rock");
+      expect(wrapper.find(Result).props().countdown).toEqual(3);
+      jest.runAllTimers();
+      expect(wrapper.find(Result).props().result).toEqual(
+        "Fair enough, it's a draw!"
+      );
+    });
+    test("scissors won over paper", () => {
+      const props = {};
+      const wrapper = Enzyme.shallow(<PlayContainer {...props} />);
+      wrapper.instance().jokenPo("scissors", "paper");
+      expect(wrapper.find(Result).props().countdown).toEqual(3);
+      jest.runAllTimers();
+      expect(wrapper.find(Result).props().result).toEqual("Nice, you won!!");
+    });
+    test("scissors draws with scissors", () => {
+      const props = {};
+      const wrapper = Enzyme.shallow(<PlayContainer {...props} />);
+      wrapper.instance().jokenPo("scissors", "scissors");
+      expect(wrapper.find(Result).props().countdown).toEqual(3);
+      jest.runAllTimers();
+      expect(wrapper.find(Result).props().result).toEqual(
+        "Fair enough, it's a draw!"
+      );
+    });
+    test("paper wins over rock", () => {
+      const props = {};
+      const wrapper = Enzyme.shallow(<PlayContainer {...props} />);
+      wrapper.instance().jokenPo("paper", "rock");
+      expect(wrapper.find(Result).props().countdown).toEqual(3);
+      jest.runAllTimers();
+      expect(wrapper.find(Result).props().result).toEqual("Nice, you won!!");
+    });
+    test("paper draws with paper", () => {
+      const props = {};
+      const wrapper = Enzyme.shallow(<PlayContainer {...props} />);
+      wrapper.instance().jokenPo("paper", "paper");
+      expect(wrapper.find(Result).props().countdown).toEqual(3);
+      jest.runAllTimers();
+      expect(wrapper.find(Result).props().result).toEqual(
+        "Fair enough, it's a draw!"
+      );
+    });
+    test("paper loses over scissors", () => {
+      const props = {};
+      const wrapper = Enzyme.shallow(<PlayContainer {...props} />);
+      wrapper.instance().jokenPo("paper", "scissors");
+      expect(wrapper.find(Result).props().countdown).toEqual(3);
+      jest.runAllTimers();
+      expect(wrapper.find(Result).props().result).toEqual("Oh no, you lost!");
+    });
+    test("rock loses over paper", () => {
+      const props = {};
+      const wrapper = Enzyme.shallow(<PlayContainer {...props} />);
+      wrapper.instance().jokenPo("rock", "paper");
+      expect(wrapper.find(Result).props().countdown).toEqual(3);
+      jest.runAllTimers();
+      expect(wrapper.find(Result).props().result).toEqual("Oh no, you lost!");
+    });
+    test("scissors loses over rock", () => {
+      const props = {};
+      const wrapper = Enzyme.shallow(<PlayContainer {...props} />);
+      wrapper.instance().jokenPo("scissors", "rock");
+      expect(wrapper.find(Result).props().countdown).toEqual(3);
+      jest.runAllTimers();
+      expect(wrapper.find(Result).props().result).toEqual("Oh no, you lost!");
+    });
+  });
+  jest.useFakeTimers();
+  test("pressButton", () => {
+    const props = {};
+    const wrapper = Enzyme.shallow(<PlayContainer {...props} />);
+    const beforeProps = wrapper.find(Result).props();
+    wrapper.instance().resetState("rock");
+    const afterProps = wrapper.find(Result).props();
+   // console.log(beforeProps, afterProps);
+    expect(afterProps.countdown).toEqual(3);
+    jest.runAllTimers();
+    const propsAfterTimeout = wrapper.find(Result).props();
+   // console.log(propsAfterTimeout);
+  });
+});
--- __tests__/rocky/containers/WatchContainer.test.js
+++ __tests__/rocky/containers/WatchContainer.test.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import React from "react";
+import Enzyme from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
-import WatchContainer from '../../../src/rocky/watch/containers/WatchContainer'
+import WatchContainer from "../../../src/rocky/watch/containers/WatchContainer";
+import WatchResult from "../../../src/rocky/watch/components/WatchResult";
 
 Enzyme.configure({ adapter: new Adapter() });
 var wrapper;
@@ -11,25 +12,113 @@ var wrapper;
 //     wrapper = Enzyme.shallow(<WatchContainer />);
 //   expect(wrapper).toMatchSnapshot();
 // })
-test('resetState', () => {
+test("resetState", () => {
   const props = {};
   const wrapper = Enzyme.shallow(<WatchContainer {...props} />);
   let action = wrapper.instance().resetState();
-  expect (action).toEqual(true);
-  
-  })
+  expect(action).toEqual(true);
+});
 
-  test('generateChoice', () => {
-    const props = {};
-    const wrapper = Enzyme.shallow(<WatchContainer {...props} />);
-    let action = wrapper.instance().generateChoice();
-    expect (action).toEqual(true);
-    
-    })  
-    test('jokenPo', () => {
+test("generateChoice", () => {
+  const props = {};
+  const wrapper = Enzyme.shallow(<WatchContainer {...props} />);
+  let action = wrapper.instance().generateChoice();
+  expect(action).toEqual(true);
+});
+
+jest.useFakeTimers();
+describe("WatchContainer Components", () => {
+  describe("jokenPo function", () => {
+    test("rock won's over scissors", () => {
       const props = {};
       const wrapper = Enzyme.shallow(<WatchContainer {...props} />);
-      let action = wrapper.instance().jokenPo();
-      expect (action).toEqual(true);
-      
-      })  
+      wrapper.instance().jokenPo("rock", "scissors");
+      expect(wrapper.find(WatchResult).props().countdown).toEqual(3);
+      jest.runAllTimers();
+      expect(wrapper.find(WatchResult).props().result).toEqual("Jack won!!");
+    });
+    test("rock draws with rock", () => {
+      const props = {};
+      const wrapper = Enzyme.shallow(<WatchContainer {...props} />);
+      wrapper.instance().jokenPo("rock", "rock");
+      expect(wrapper.find(WatchResult).props().countdown).toEqual(3);
+      jest.runAllTimers();
+      expect(wrapper.find(WatchResult).props().result).toEqual(
+        "Fair enough, it's a draw!"
+      );
+    });
+    test("scissors won over paper", () => {
+      const props = {};
+      const wrapper = Enzyme.shallow(<WatchContainer {...props} />);
+      wrapper.instance().jokenPo("scissors", "paper");
+      expect(wrapper.find(WatchResult).props().countdown).toEqual(3);
+      jest.runAllTimers();
+      expect(wrapper.find(WatchResult).props().result).toEqual("Jack won!!");
+    });
+    test("scissors draws with scissors", () => {
+      const props = {};
+      const wrapper = Enzyme.shallow(<WatchContainer {...props} />);
+      wrapper.instance().jokenPo("scissors", "scissors");
+      expect(wrapper.find(WatchResult).props().countdown).toEqual(3);
+      jest.runAllTimers();
+      expect(wrapper.find(WatchResult).props().result).toEqual(
+        "Fair enough, it's a draw!"
+      );
+    });
+    test("paper wins over rock", () => {
+      const props = {};
+      const wrapper = Enzyme.shallow(<WatchContainer {...props} />);
+      wrapper.instance().jokenPo("paper", "rock");
+      expect(wrapper.find(WatchResult).props().countdown).toEqual(3);
+      jest.runAllTimers();
+      expect(wrapper.find(WatchResult).props().result).toEqual("Jack won!!");
+    });
+    test("paper draws with paper", () => {
+      const props = {};
+      const wrapper = Enzyme.shallow(<WatchContainer {...props} />);
+      wrapper.instance().jokenPo("paper", "paper");
+      expect(wrapper.find(WatchResult).props().countdown).toEqual(3);
+      jest.runAllTimers();
+      expect(wrapper.find(WatchResult).props().result).toEqual(
+        "Fair enough, it's a draw!"
+      );
+    });
+    test("paper loses over scissors", () => {
+      const props = {};
+      const wrapper = Enzyme.shallow(<WatchContainer {...props} />);
+      wrapper.instance().jokenPo("paper", "scissors");
+      expect(wrapper.find(WatchResult).props().countdown).toEqual(3);
+      jest.runAllTimers();
+      expect(wrapper.find(WatchResult).props().result).toEqual("Jade won!!");
+    });
+    test("rock loses over paper", () => {
+      const props = {};
+      const wrapper = Enzyme.shallow(<WatchContainer {...props} />);
+      wrapper.instance().jokenPo("rock", "paper");
+      expect(wrapper.find(WatchResult).props().countdown).toEqual(3);
+      jest.runAllTimers();
+      expect(wrapper.find(WatchResult).props().result).toEqual("Jade won!!");
+    });
+    test("scissors loses over rock", () => {
+      const props = {};
+      const wrapper = Enzyme.shallow(<WatchContainer {...props} />);
+      wrapper.instance().jokenPo("scissors", "rock");
+      expect(wrapper.find(WatchResult).props().countdown).toEqual(3);
+      jest.runAllTimers();
+      expect(wrapper.find(WatchResult).props().result).toEqual("Jade won!!");
+    });
+  });
+  jest.useFakeTimers();
+  test("pressButton", () => {
+    const props = {};
+    const wrapper = Enzyme.shallow(<WatchContainer {...props} />);
+    const beforeProps = wrapper.find(WatchResult).props();
+    wrapper.instance().resetState("rock");
+    const afterProps = wrapper.find(WatchResult).props();
+   // console.log(beforeProps, afterProps);
+    expect(afterProps.countdown).toEqual(3);
+    jest.runAllTimers();
+    const propsAfterTimeout = wrapper.find(WatchResult).props();
+   // console.log(propsAfterTimeout);
+  });
+});
